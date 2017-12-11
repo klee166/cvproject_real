848f0719c37ea8a9c624b5a089906d54a42809a9
Kwang <klee166@jhu.edu>
2017-12-10 21:29:22 -0500

    rotation file

---
+++ rotate.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import logging
+import argparse
+import cv2
+import numpy as np
+
+# add argument
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description="CV assignment")
+parser.add_argument("--input", default=0, type=str, required=True,
+                    help="Set if it is cuda or not (0 = not cuda)(1 = cuda)")
+parser.add_argument("--rotate", default=0, type=int, required=True,
+                    help="Set if it is cuda or not (0 = not cuda)(1 = cuda)")
+parser.add_argument("--save", default=0, type=int, required=True,
+                    help="Set if it is cuda or not (0 = not cuda)(1 = cuda)")
+
+
+args = parser.parse_args()
+
+
+im = cv2.imread(args.input,0)
+rows, cols = im.shape
+print im
+
+M = cv2.getRotationMatrix2D((cols/2, rows/2),args.rotate,1)
+dst = cv2.warpAffine(im, M,(cols,rows))
+if args.save == 1:
+	cv2.imwrite(args.input, dst)
